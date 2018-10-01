561ae0aef8b663e0618347fe9ebb04cf333e4d31
kuhadiyadharmesh@gmail.com <kuhadiyadharmesh@gmail.com>
2018-10-01 20:03:49 +0530

    0.0.0.1

---
+++ src/main/java/recapi/lib/netd/MyTestClass.java
@@ -0,0 +1,13 @@
+package recapi.lib.netd;
+
+/**
+ * Created by Dharmesh-PC on 10/1/2018.
+ */
+
+public class MyTestClass
+{
+    public String MyClass()
+    {
+        return "testing";
+    }
+}
